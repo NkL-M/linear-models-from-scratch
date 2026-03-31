0c9b4ff27a9616195d6236457e5788fcb5967c68
Nicolas Marechal <marechal.n@hotmail.com>
2026-03-31 20:47:17 +0200

    Module Docstring added

--- linear_models/utils.py
+++ linear_models/utils.py
@@ -1,27 +1,11 @@
+"""
+Module for plots
+"""
+
 import matplotlib.pyplot as plt
 import numpy as np
-#TODO - Create Seaborn or Plotly.express version of these functions
-# import seaborn as sns
-# import plotly.express as px
 
-
-def old_plot(loss_train_history, loss_val_history):
-    # Plot train and test histories
-    plt.plot(loss_train_history, label = 'training loss')
-    plt.plot(loss_val_history, label = 'validation loss')
-
-    # Set title and labels
-    plt.title('Learning Curves Train/Val Loss - Hand-Made Model')
-    plt.ylabel('MSE Loss')
-    plt.xlabel('Epochs')
-
-    # Change limits
-    plt.ylim(ymin = 00, ymax = 15_000)
-
-    # Generate legend
-    plt.legend()
-
-def plot_loss(history):
+def plot_loss(history, baseline):
     """
     Function to plot the loss function history
 
@@ -29,25 +13,21 @@ def plot_loss(history):
     ----
     Plot
     """
-    # --- LOSS --- #
-    plt.figure(figsize=(7,4))
+    plt.figure(figsize=(15,7))
     plt.plot(history['loss_train_history'])
     plt.plot(history['loss_val_history'])
+    plt.axhline(y=baseline, color='red', linestyle='--', linewidth=0.8, label='aze')
 
     plt.title('Model loss')
     plt.ylabel('Loss')
     plt.xlabel('Epoch')
 
-    # inf = np.min(history['loss_train_history'])#-1000
-    # sup = np.max(history['loss_train_history']) + (np.max(history['loss_train_history']) * 1.1)
-    # plt.ylim((inf, sup))
-
-    plt.legend(['Training loss', 'Validation loss'], loc='best')
+    plt.legend(['Training loss', 'Validation loss', 'Baseline'], loc='best')
 
     plt.grid(axis="x",linewidth=0.5)
     plt.grid(axis="y",linewidth=0.5)
 
-def plot_loss_metric(history):
+def plot_loss_metric(history, baseline):
     """
     Function to plot the loss and metric histories
 
@@ -65,14 +45,14 @@ def plot_loss_metric(history):
     ax[0].set_ylabel('Loss')
     ax[0].set_xlabel('Epoch')
 
-    # ax[0].set_ylim((0,3))
+    ax[0].hlines(y=baseline, xmin=-20, xmax=len(history['loss_train_history']), colors='red', linestyles='--', linewidth=0.8)
 
-    ax[0].legend(['Train', 'Val'], loc='best')
+    ax[0].legend(['Training', 'Validation', 'Basline'], loc='best')
 
     ax[0].grid(axis="x",linewidth=0.5)
     ax[0].grid(axis="y",linewidth=0.5)
 
-    # # --- METRIC --- #
+    # --- METRIC --- # TODO Test with metrics in Logistic Regression
     # ax[1].plot(history['metric_train_history'])
     # ax[1].plot(history['metric_val_history'])
 
@@ -80,9 +60,9 @@ def plot_loss_metric(history):
     ax[1].set_ylabel('Metric')
     ax[1].set_xlabel('Epoch')
 
-    ax[1].legend(['Train', 'Val'], loc='best')
+    ax[1].legend(['Training', 'Validation', 'Basline'], loc='best')
 
-    # ax[1].ylim((0,1))
+    ax[1].hlines(y=baseline, xmin=-20, xmax=len(history['loss_train_history']), colors='red', linestyles='--', linewidth=0.8)
 
     ax[1].grid(axis="x",linewidth=0.5)
     ax[1].grid(axis="y",linewidth=0.5)
